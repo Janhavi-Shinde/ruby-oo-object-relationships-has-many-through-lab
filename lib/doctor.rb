898ac8d173da0575b3d5bd494dbe280726a8e7f4
janhavi <janashinde394@gmail.com>
2022-05-25 01:11:07 +1000

    all tests passing

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,1 +1,28 @@
+class Doctor
+attr_accessor :name
 
+@@all = []
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+    def self.all
+        @@all
+    end
+
+    def appointments
+        Appointment.all.select {|instance| instance.doctor == self}
+    end
+
+    def new_appointment(date, patient)
+        Appointment.new(date, patient, self)
+    end
+
+    def patients
+        appointments.collect {|instance| instance.patient}
+    end
+
+    
+
+end
